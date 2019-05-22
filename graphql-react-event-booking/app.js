b1c5afebb7de9278bd49c7a1bb8b22711608745e
Preetham Sridhar <preetham.prem2325@gmail.com>
2019-05-22 14:04:56 +0530

    Types and data, adding the query and mutation

--- graphql-react-event-booking/app.js
+++ graphql-react-event-booking/app.js
@@ -6,28 +6,53 @@ const { buildSchema } = require('graphql')
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 app.use(
     '/graphql',
     graphqlHttp({
         schema: buildSchema(`
-        type RootQuery {
-            events: [String!]!
-        }
-        type RootMutation{
-            createEvent(name: String): String
-        }
-        schema {
-            query: RootQuery
-            mutation: RootMutation
-        }
-    `),
-        rootValue: {
+            type Event {
+                _id: ID!
+                title: String!
+                description: String!
+                price: Float!
+                date: String!
+            }
+
+            input EventInput {
+                title: String!
+                description: String!
+                price: Float!
+                date: String!    
+            }
+
+            type RootQuery {
+                events: [Event!]!
+            }
+            type RootMutation{
+                createEvent(eventInput: EventInput): Event
+            }  
+            schema {
+                query: RootQuery
+                mutation: RootMutation
+            }
+        `),
+        rootValue: { //bundle of all the things 
             events: () => {
-                return ['Romantic cooking', 'coding', 'all night coding'];
+                return events; 
             },
             createEvent: (args) => {
-                const eventName = args.name;
-                return eventName;
+                const event = {
+                    _id: Math.random().toString(),
+                    title: args.eventInput.title,
+                    description: args.eventInput.description,
+                    price: +args.eventInput.price,
+                    data: args.eventInput.date
+                }; 
+                events.push(event);
+                console.log(events)
+                return event;
             }
         },
         graphiql: true
